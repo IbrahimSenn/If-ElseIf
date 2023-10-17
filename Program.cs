b58c7c49cc8ced914fa53f91e864b814b184a6d6
Monsterter <ibrahimsenc@gmail.com>
2023-10-17 15:51:06 +0300

    Second Commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int time = DateTime.Now.Hour;
+
+if ( time>=6 && time<11 )
+
+    Console.WriteLine("günaydın");
+else if( time<=18 )
+
+    Console.WriteLine("iyi günler");
+else
+
+    Console.WriteLine("iyi geceler");
+
+string sonuc = time<=18 ? "İyi günler" : "İyi geceler" ;
+
+    Console.WriteLine(sonuc );
